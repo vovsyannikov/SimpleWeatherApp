85bcd7e9fc591664a0c270922a29aec764f6604b
Виталий Овсянников <its.vint@ya.ru>
2025-02-28 16:34:27 +0300

    Initial Commit

---
+++ SimpleWeatherApp/SimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherAppApp.swift
+//  SimpleWeatherApp
+//
+//  Created by Виталий Овсянников on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
